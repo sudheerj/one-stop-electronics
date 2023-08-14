9646ec3fb625f82d3f3a9900a536aa63cd639b5c
Sudheer Jonna <jsudheer50@gmail.com>
2023-08-14 19:07:48 +0530

    Move firebase config to environment file

--- src/backend/firebase/config.ts
+++ src/backend/firebase/config.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app"
 import { GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBldF8hTd2K51dbn_AGGTNkrfMcPV4VKjA",
-  authDomain: "onestop-electronics.firebaseapp.com",
-  projectId: "onestop-electronics",
-  storageBucket: "onestop-electronics.appspot.com",
-  messagingSenderId: "388971771085",
-  appId: "1:388971771085:web:9dd330d39ce7a10d68a584",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 export const firebaseApp = initializeApp(firebaseConfig)
